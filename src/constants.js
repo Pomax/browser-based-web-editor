1f1f376e93a186d1e4bfdc8f60666ef88d2449da
undefined <actions@browsertests.local>
2025-08-15 13:43:57 -0700

    ...what was this for?

--- src/constants.js
+++
@@ -1,5 +0,0 @@
-import { fileURLToPath } from "url";
-import { dirname, sep, posix } from "path";
-export const __dirname = dirname(fileURLToPath(import.meta.url))
-  .replaceAll(sep, posix.sep)
-  .replace(`/src`, ``);
