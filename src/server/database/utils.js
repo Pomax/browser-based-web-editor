73774e293dfa447729f2e087827ed92f0eda5cb4
Pomax <pomax@nihongoresources.com>
2025-08-27 13:12:46 -0700

    .

--- src/server/database/utils.js
+++ src/server/database/utils.js
@@ -2,9 +2,10 @@
  * ...docs go here...
  */
 export function composeWhere(where, suffix = []) {
+  let ua = where.updated_at;
+  if (where.updated_at) delete where.updated_at;
   let filter = Object.entries(where)
     .map(([k, v]) => {
-      if (k === `updated_at`) return false;
       if (v === null || v === undefined) {
         suffix.push(`${k} IS NULL`);
         return false;
@@ -17,5 +18,6 @@ export function composeWhere(where, suffix = []) {
   const values = Object.values(where).filter(
     (v) => !(v === undefined || v === null)
   );
+  if (ua) where.updated_at = ua;
   return { filter, values };
 }
